7dfbc34dd5585f3449d6af0df256bdc10e43917d
care1e55 <>
2024-03-03 21:42:23 +0100

    add: chat

--- src/pages/pages/chat.py
+++ src/pages/pages/chat.py
@@ -13,8 +13,41 @@ class Message:
         self.state = None
 
 
+def render_messages():
+    if "messages" not in st.session_state:
+        st.session_state.messages = []
+    for message in st.session_state.messages:
+        with st.chat_message(message["role"]):
+            st.markdown(message["content"])
+
+
+def render_message(user, message):
+    st.session_state.messages.append(
+        {"role": user, "content": message}
+    )
+    with st.chat_message(user):
+        st.markdown(st.session_state.messages[-1]["content"])
+
+
+def response(dialog, message):
+    dialog.state_prompt(message)
+    with st.chat_message("assistant"):
+        _response = st.write_stream(
+            dialog.client.chat.completions.create(
+                model=dialog.settings.openai_model,
+                messages=[dialog.prompt],
+                stream=True,
+            )
+        )
+    st.session_state.messages.append(
+        {"role": "assistant", "content": _response}
+    )
+
+
 def chat_page():
     st.title("👩‍🚀 Chat")
-    dialog = get_dialog().render_messages()
+    dialog = get_dialog()
+    render_messages()
     if message := st.chat_input("Message"):
-        dialog.response(message)
+        render_message("user", message)
+        response(dialog, message)
